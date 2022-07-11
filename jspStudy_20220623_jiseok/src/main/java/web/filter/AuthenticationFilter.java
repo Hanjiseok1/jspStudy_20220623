99fdaa78db98669f73d63102cff3a86066e28a28
Hanjiseok1 <heemoon25@naver.com>
2022-07-11 21:51:17 +0900

    서블릿 마무리

--- jspStudy_20220623_jiseok/src/main/java/web/filter/AuthenticationFilter.java
+++ jspStudy_20220623_jiseok/src/main/java/web/filter/AuthenticationFilter.java
@@ -42,12 +42,13 @@ public class AuthenticationFilter extends HttpFilter implements Filter {
 				return;
 			}
 		}
+		req.getServletContext();
 		
 		chain.doFilter(request, response);
 	}
 
 	public void init(FilterConfig fConfig) throws ServletException {
-
+		fConfig.getServletContext();
 	}
 
 }
